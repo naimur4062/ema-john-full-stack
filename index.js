f40fb28edfab707969e6f215a9a0db8e6bcf94fb
naimur4062 <naimur4062@gmail.com>
2021-03-29 00:10:52 +0800

    update

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 5000
 
+app.get('/', (req, res) => {
+    res.send('database is working.')
+})
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
